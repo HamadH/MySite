489f465d3dd4eb7d38160b3889aac85b53ec015b
HamadH <hmyhafiz@gmail.com>
2016-04-29 19:25:28 -0400

    Added files via upload

--- js/main.js
+++ js/main.js
@@ -1,1 +1,70 @@
+﻿
+
+var app = angular.module('myApp', ['ngGrid']);
+app.controller('MyCtrl', function($scope) {
+    $scope.gridOptions = {
+        data: 'myData',
+        enablePinning: true,
+        enableRowSelection: true,
+        selectedItems: $scope.selections,
+        afterSelectionChange:function() {
+            if ($scope.selections != "" ) {
+                $scope.disabled = false;
+                alert($scope.selectRow.name);
+            } else {
+                alert("true");
+                $scope.disabled = true;
+            }
+        
+    },
+        columnDefs: [{ field: "name", width: 120, pinned: true },
+                    { field: "age", width: 120 },
+                    { field: "birthday", width: 120 },
+                    { field: "salary", width: 120 }]
+    };
+
+
+    $scope.myData = [{ name: "Moroni", age: 50, birthday: "Oct 28, 1970", salary: "60,000" },
+                    { name: "Tiancum", age: 43, birthday: "Feb 12, 1985", salary: "70,000" },
+                    { name: "Jacob", age: 27, birthday: "Aug 23, 1983", salary: "50,000" },
+                    { name: "Nephi", age: 29, birthday: "May 31, 2010", salary: "40,000" },
+                    { name: "Enos", age: 34, birthday: "Aug 3, 2008", salary: "30,000" },
+                    { name: "Moroni", age: 50, birthday: "Oct 28, 1970", salary: "60,000" },
+                    { name: "Tiancum", age: 43, birthday: "Feb 12, 1985", salary: "70,000" },
+                    { name: "Jacob", age: 27, birthday: "Aug 23, 1983", salary: "40,000" },
+                    { name: "Nephi", age: 29, birthday: "May 31, 2010", salary: "50,000" },
+                    { name: "Enos", age: 34, birthday: "Aug 3, 2008", salary: "30,000" },
+                    { name: "Moroni", age: 50, birthday: "Oct 28, 1970", salary: "60,000" },
+                    { name: "Tiancum", age: 43, birthday: "Feb 12, 1985", salary: "70,000" },
+                    { name: "Jacob", age: 27, birthday: "Aug 23, 1983", salary: "40,000" },
+                    { name: "Nephi", age: 29, birthday: "May 31, 2010", salary: "50,000" },
+                    { name: "Enos", age: 34, birthday: "Aug 3, 2008", salary: "30,000" }];
+
+   
+    
+
+   
+
+    $scope.$on('ngGridEventData', function () {
+        alert(data.name)
+        $scope.gridOptions.selectRow(0, true);
+        alert(data.name)
+    });
+
+
+    $scope.selectEnosRow = function () {
+        angular.forEach($scope.myData, function (data, index) {
+            alert(data.name)
+            if (data.name == 'Enos') {
+                $scope.gridOptions.selectItem(index, true);
+              
+            }
+        });
+    };
+
+
+});
+
+
+
 
